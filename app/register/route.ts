deaf9e1be8e714c809f0d5e35f7a38b473a4506d
Toby Liu <toby.liu@hulu.com>
2024-04-11 11:49:50 -0700

    Try a GET route

--- app/register/route.ts
+++ app/register/route.ts
@@ -1,3 +1,5 @@
+import querystring from 'node:querystring';
+
 export const dynamic = 'force-dynamic'
 
 export async function POST(request: Request) {
@@ -7,6 +9,32 @@ export async function POST(request: Request) {
     const payload = await request.json();
 
     console.log(payload)
+
+    return Response.json({
+        hi: true
+    }, {
+        status: 201
+    })
+}
+
+export async function GET(request: Request) {
+
+    // TODO: store the subscription
+    const urlParams = new URLSearchParams(request.url);
+    const endpoint = urlParams.get('endpoint');
+    const p256dh = urlParams.get('p256dh');
+    const auth = urlParams.get('auth');
+    
+    const subscription = {
+        subscription: {
+            endpoint,
+            expirationTime: null,
+            keys: {
+                p256dh,
+                auth,
+            }
+        }
+    }
 
     return Response.json({
         hi: true
--- next.config.js
+++ next.config.js
@@ -6,10 +6,7 @@ module.exports = {
           source: "/:path*",
           headers: [
             { key: "Access-Control-Allow-Credentials", value: "true" },
-            { key: "Access-Control-Allow-Origin", value: "https://web-push-demo-virid.vercel.app/" },
-            { key: "Access-Control-Allow-Origin", value: "https://localhost:3000" },
             { key: "Access-Control-Allow-Origin", value: "https://local.disneyplus.com:3000" },
-            { key: "Access-Control-Allow-Origin", value: "https://stage-web.disneyplus.com" },
             { key: "Access-Control-Allow-Methods", value: "GET,OPTIONS,PATCH,DELETE,POST,PUT" },
             { key: "Access-Control-Allow-Headers", value: "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version" },
           ]
